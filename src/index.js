b359ad01249e52404fd92ebfdaaf395d54947680
Vaibhav Kumar <rishunam8@gmail.com>
2025-09-06 02:22:12 +0530

    Switch BrowserRouter to HashRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { HashRouter } from "react-router-dom";  // ✅ Import HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
